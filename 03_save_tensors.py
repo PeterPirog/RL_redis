fb99ff5741d16b8fc2eae80bdc86d4211d08b23d
PeterPirog <peterpirogtf@gmail.com>
2020-12-03 22:11:38 +0100

    redis tests

--- 03_save_tensors.py
+++ 03_save_tensors.py
@@ -2,45 +2,112 @@ import numpy as np
 import gym
 import tensorflow as tf
 from redisai import Client
-client = Client(host='192.168.1.16',port=6380,db=0)
-
-def policy_pi(observation,n_actions=4):
-    action=np.random.randint(2)
-    #print('action=',action)
-    return action
+import re
+import time
 
 
-if __name__ == '__main__':
+class TrainerInterface(Client):
+    def __init__(self,host,port,db=0):
+        super().__init__()
+        #redis data
+        self.host=host
+        self.port=port
+        self.db = db
+        self.client = Client(host=self.host, port=self.port, db=self.db) #decode_responses=True
 
-    env = gym.make('CartPole-v0')
-    obs = env.reset()
+        # Environment data
+        self.env_name=str(self.client.get('env_name'),encoding='utf-8')
+        self.input_dims=self.client.tensorget('input_dims')
+        self.n_actions=int(self.client.get('n_actions'))
+        self.action_continous=int(self.client.get('action_continous'))
+        self.max_action=float(self.client.get('max_action')) #maximum value for action output
+        self.min_action = float(self.client.get('min_action'))#minimum value for action output
+
+        #Prcess control data
+        self.stop_gathering=int(self.client.get('stop_gathering')) #information for regulators if 1 stop data collecting
+        self.mem_cntr = self.client.get('mem_cntr')
+        self.mem_size = float(self.client.get('mem_size'))
+
+        self.show_info()
+
+    def show_info(self):
+        print('\n ---------------- INFO ---------------------------------')
+        print(f'The environment: {self.env_name} has been created\n')
+        print(' -------------- observations --------------------------')
+        print(f'Input observation dimension: {self.input_dims}\n')
+        print(' -------------- actions --------------------------')
+        print(f'Number of actions: {self.n_actions}')
+        print(f'Action continous: {self.action_continous}')
+        print(f'Maximum action value: {self.max_action}')
+        print(f'Minimum action value: {self.min_action}')
+        try:
+            print(f'Action meanings: {self.env.unwrapped.get_action_meanings()}')
+        except:
+            print(f'Action meanings not decribed in env')
+
+    def return_batch(self,batch_size):
+        pass
+        #return observations,actions,rewards,observations_,dones
+
+if __name__=='__main__':
+    tr_i=TrainerInterface(host='192.168.1.16',port=6379)
 
 
-    # action=pi(obs)
+    batch_size=5
+    start = time.time()   #time count
+    observations=np.zeros(shape=(batch_size,24))
+    observations_ = np.zeros(shape=(batch_size, 24))
+    actions=np.zeros(shape=(batch_size, 4))
+    rewards=np.zeros(shape=(batch_size))
+    dones = np.zeros(shape=(batch_size))
 
-    totals = []
-    done = False
-    for episode in range(10):
-        episode_rewards = 0
-        obs = env.reset()
-        env.render()
+    trajectories=tr_i.client.srandmember('trajectories',batch_size)
+    for i in range(batch_size):
+        trajectory=trajectories[i]
+        #print(trajectory)
 
-        for step in range(200):
-            action = policy_pi(obs)
-            obs, reward, done, info = env.step(action)
+        number=re.findall(r'\d+', str(trajectory,encoding='utf-8'))[0]
+        #print('number=',number)
+        sarsd=tr_i.client.smembers(trajectory)
+        #print(sarsd)
+        reward,done=tr_i.client.mget(f'reward{number}',f'done{number}')
+        #reward=float(reward)
+        done=int(done)
 
-            client.tensorset('observation', obs)
-            #print(client.tensorget('observation'))
+        obs=np.array([tr_i.client.tensorget(f'obs{number}')])
+        obs_ = np.array([tr_i.client.tensorget(f'obs_{number}')])
+        action = np.array([tr_i.client.tensorget(f'action{number}')])
+        #print(f'obs_= {obs_}, reward={reward}  done={done} ')
 
-            #client.tensorset('sars', [obs,action])
-            client.lpush('mylist', obs)
-            print(client.lpop('mylist'))
+        dones[i]=done
+        rewards[i]=reward
+        observations[i,]=obs
+        observations_[i,] = obs_
+        actions[i,]=action
 
-            env.render()
-            episode_rewards += reward
-            if done:
-                break
-        print(f'Episode reward {episode_rewards}')
+    observations=tf.convert_to_tensor(observations,dtype=tf.float32)
+    observations_ = tf.convert_to_tensor(observations_,dtype=tf.float32)
+    actions = tf.convert_to_tensor(actions,dtype=tf.float32)
+    rewards = tf.convert_to_tensor(rewards,dtype=tf.float32)
+    dones = tf.convert_to_tensor(dones,dtype=tf.int32)
+
+    end = time.time()
+
+    print('dones=',dones)
+    print('rewards=', rewards)
+    print('observations=', observations)
+    print('observations_=', observations_)
+    print('actions=', actions)
+    print('time delay',end-start)
 
 
-#print(client.keys())
+    """
+    batch1=tr_i.client.srandmember('trajectories',batch_size)
+    print(batch1)
+
+    batch2=tr_i.client.smembers('trajectory235216')
+    print(batch2)
+
+    batch3=tr_i.client.pipeline() mget(batch2)
+    print(batch3)
+    """
