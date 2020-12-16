7d38336266e4a6e24aa5c55b3ce5702768ee7b1c
PeterPirog <peterpirogtf@gmail.com>
2020-12-16 15:25:10 +0100

    get batch from redis

--- 05_load_membuffer.py
+++ 05_load_membuffer.py
@@ -1,8 +1,18 @@
 import pickle
+import numpy as np
 
 if __name__ == '__main__':
-
+    """
     with open('trajectories.pickle', 'rb') as f:
         observations, actions, rewards, observations_, dones = pickle.load(f)
 
         print(actions)
+        
+    """
+    batches=2
+    state_shape=5
+
+    obs=np.random.randn(batches,5)
+    action=np.random.randn(batches,3)
+    print('obs=',obs)
+    print('action=', action)
